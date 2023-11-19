1c421393c322440ed79ef321e31435c5998f3bba
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:29:22 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Cubic Yards Calculator by Calculator.iO
+Plugin Name: Cubic Yards Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/cubic-yards-calculator/
 Description: Cubic yard calculator calculates cubic yards, feet, meters for landscape materials. Enter measurements in yards and feet and get volume conversions.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Cubic Yards Ca
 
 function display_ci_cubic_yards_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/cubic-yards-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Cubic Yards Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_cubic_yards_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Cubic Yards Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_cubic_yards_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_cubic_yards_calculator', 'display_ci_cubic_yards_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Cubic yard calculator calculates cubic yards, feet, meters for landscape materia
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/cubic-yards-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /cubic-yards-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Cubic Yards Calculator](https://www.calculator.io/cubic-yards-calculator/ "Cubic Yards Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
